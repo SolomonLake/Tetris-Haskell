85f784f4b050e9bf20f74df95dc001a46344a267
sgiffenh@haverford.edu <sgiffenh@wasat.cs.brynmawr.edu>
2017-04-19 11:29:31 -0400

    new

--- Tetris.hs
+++ Tetris.hs
@@ -1,4 +1,62 @@
+{-
+Creator: Lake Giffen-Hunter
+Email  : sgiffenh@haverford.edu
+
+Sources:
+tetris.wikia.com -- very helpful!!
+-}
+
 module Tetris where
 
 import Graphics.Gloss.Interface.Pure.Game
 import Data.Monoid
+import Data.Vector
+
+stageWidth, stageHeight :: Int
+stageWidth = 300
+stageHeight = 500
+
+stageWidthF, stageHeightF :: Float
+stageWidthF = fromIntegral stageWidth
+stageHeightF = fromIntegral stageHeight
+
+pieceSpeed :: Float
+pieceSpeed = 1
+
+data Piece = Piece { color :: Color
+                   , rotation :: 
+                   
+
+
+{-
+Pieces:
+T:
+ @
+@X@
+
+I:
+@@@@
+rotates around center of 4x4 array
+
+O:
+@@
+@@
+
+S:
+ @@
+@X
+
+Z:
+@@
+ X@
+ 
+J:
+@
+@X@
+
+L:
+  @
+@X@
+
+
+-}
